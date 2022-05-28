193e5c8acba8f8c93c3d4c99a924eceedc7dc33b
Hoàng Trần <46571690+tlhoang1211@users.noreply.github.com>
2022-05-28 14:02:26 +0700

    1

--- src/main/java/com/example/hfb/HFBApplication.java
+++ src/main/java/com/example/hfb/HFBApplication.java
@@ -28,31 +28,31 @@ public class HFBApplication {
     CommandLineRunner runner(SeedingService service) {
         return args -> {
             // delete user_role
-//            service.deleteUserRoles();
-//            service.resetIdUserRole();
-//            // delete user
-//            service.deleteUsers();
-//            service.resetIdUser();
-//            // delete role
-//            service.deleteRoles();
-//            service.resetIdRole();
-//            // delete food
-//            service.deleteFoods();
-//            service.resetIdFood();
-//            // delete category
-//            service.deleteCategories();
-//            service.resetIdCategory();
+            service.deleteUserRoles();
+            service.resetIdUserRole();
+            // delete user
+            service.deleteUsers();
+            service.resetIdUser();
+            // delete role
+            service.deleteRoles();
+            service.resetIdRole();
+            // delete food
+            service.deleteFoods();
+            service.resetIdFood();
+            // delete category
+            service.deleteCategories();
+            service.resetIdCategory();
             long datetime = Calendar.getInstance().getTimeInMillis();
             // add role
             service.saveRole(new Role(1, "ROLE_ADMIN"));
             service.saveRole(new Role(2, "ROLE_USER"));
 
            // add user
-            service.saveUser(new User(1, "admin", "admin@gmail.com", "123", "admin@gmail.com", "09061234", "Ha noi", "", 100, datetime, datetime, 1, 1, 1));
-            service.saveUser(new User(2, "Tran Hoang", "tlhoang1211@gmail.com", "123", "tlhoang1211@gmail.com", "09061234", "Ha noi", "", 100, datetime, datetime, 1, 1, 1));
-            service.saveUser(new User(3, "A", "test1@gmail.com", "123", "test1@gmail.com", "09061234", "Ha noi", "", 100, datetime, datetime, 1, 1, 1));
-            service.saveUser(new User(4, "B", "test2@gmail.com", "123", "test2@gmail.com", "09061234", "Ha noi", "", 100, datetime, datetime, 1, 1, 1));
-            service.saveUser(new User(5, "C", "test3@gmail.com", "123", "test3@gmail.com", "09061234", "Ha noi", "", 100, datetime, datetime, 1, 1, 1));
+            service.saveUser(new User(1, "admin", "admin@gmail.com", "123", "admin@gmail.com", "09061234", "Ha noi", "https://res.cloudinary.com/vernom/image/upload/v1636104927/hanoi_food_bank_project/users_avatar/admin.jpg", 100, datetime, datetime, 1, 1, 1));
+            service.saveUser(new User(2, "Tran Hoang", "tlhoang1211@gmail.com", "123", "tlhoang1211@gmail.com", "09061234", "Ha noi", "https://res.cloudinary.com/vernom/image/upload/v1636303335/hanoi_food_bank_project/users_avatar/user1.jpg", 100, datetime, datetime, 1, 1, 1));
+            service.saveUser(new User(3, "A", "test1@gmail.com", "123", "test1@gmail.com", "09061234", "Ha noi", "https://res.cloudinary.com/vernom/image/upload/v1636105006/hanoi_food_bank_project/users_avatar/food%20donor.png", 100, datetime, datetime, 1, 1, 1));
+            service.saveUser(new User(4, "B", "test2@gmail.com", "123", "test2@gmail.com", "09061234", "Ha noi", "https://res.cloudinary.com/vernom/image/upload/v1636104837/hanoi_food_bank_project/users_avatar/volunteer.png", 100, datetime, datetime, 1, 1, 1));
+            service.saveUser(new User(5, "C", "test3@gmail.com", "123", "test3@gmail.com", "09061234", "Ha noi", "https://res.cloudinary.com/vernom/image/upload/v1636105076/hanoi_food_bank_project/users_avatar/recipient.jpg", 100, datetime, datetime, 1, 1, 1));
 
             //add user_role
             service.addRoleToUser("admin@gmail.com", "ROLE_ADMIN");
